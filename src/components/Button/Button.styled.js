257d194fa8c36fde22203521469802e731de470e
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-03-31 08:36:31 +0300

    Update Button.styled.js

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const ButtonStyled = styled.button`
+  display: inline-block;
   min-width: 180px;
   padding: 8px 16px;
   border-radius: 4px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
-  display: inline-block;
   color: #fff;
   border: 0;
   text-decoration: none;
